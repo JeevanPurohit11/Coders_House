94b5e3d222c441a21add3a3362c82f442ef17cca
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-06 23:41:29 +0530

    enable middlever in express

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ require('dotenv').config();  // (1) We can use all credentials available in .env
 const express = require('express');  // (2) Imported express
 const app = express();  // (3) Called
 const PORT = process.env.PORT || 5500; // (4) If given port by env is not available, run our server on PORT 5500
+const bodyParser = require('body-parser');  //imported
 
 // Creating the route. Rather than giving logic here, I will implement logic in another file and use it here.
 app.get('/', (req, res) => {
   res.send('Hello from express Js');
 });
+
+app.use(bodyParser.json());  // enable json middlever ,which come inbuild in express
 
 // Import router
 const router = require('./routes');
