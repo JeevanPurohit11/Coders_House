c14587256b24a55fef0e70a9c6554a5b4f98b1f2
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-06 21:45:19 +0530

    all routes stored globally

--- backend/routes.js
+++ backend/routes.js
@@ -1,1 +1,12 @@
-hello
+//all our routes will be store here
+//creating router (Router is bascially a function)
+const router=require('express').Router();   // our express router has been created inside router.
+const authController = require('./Controllers/auth-controller');  //imported
+
+
+ //our first request would be post request.
+router.post('/api/send-otp', authController.sendOtp);
+
+
+
+module.exports=router;
