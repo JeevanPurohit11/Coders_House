dc58bf84ce659f16384cf92f44bd27d6391ed2bd
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-08 22:28:19 +0530

    schema created

---
+++ backend/models/user-model.js
@@ -0,0 +1,12 @@
+const moongose = require('mongoose');
+const Schema = moongose.Schema;
+
+//schema gives blueprint of how our data store in our database;(created schema)
+const userSchema= new Schema({
+    phone : {type : String , required : true},
+    activated : { type : Boolean , required : false , default : false}
+},{
+    timestamps: true;
+});
+                           //model Name , used Schema , table/connection name
+module.exports = moongose.model('User',userSchema,'users');
