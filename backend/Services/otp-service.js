8de196c800c833a91d5455ba5da38975f237002a
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-08 17:04:34 +0530

    imported dependency

--- backend/Services/otp-service.js
+++ backend/Services/otp-service.js
@@ -1,12 +1,24 @@
 const crypto = require("crypto");
 
+
+const smsSid=process.env.smsSid; 
+const smsAuthToken=process.env.smsAuthToken;
+const twilio=require('twilio')(smsSid,smsAuthToken,{
+    lazyLoading: true              // writen for obtimized working.
+})
 class otpServices{
     
    async generateOtp(){    
         const otp=crypto.randomInt(1000,9999);
         return otp;     // return promise
     }
-    sendBySms() {}
+    async sendBySms(phone ,otp ) {
+        return await twilio.messages.create({
+           to : phone,
+           from : process.env.SMS_FROM_NUMBER,
+           body : `Your COderHouse project started sending otp to Rakesh :  ${otp}`,
+        });
+    }
 
     verifyOtp() {}
 }
