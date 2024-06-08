5fedd5338ef1439a2da55ee9c89a852d8c37177b
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-08 19:24:22 +0530

    created

---
+++ backend/database.js
@@ -0,0 +1,16 @@
+const mongoose= require('mongoose');
+
+function DbConnect(){
+   const DB_URL = process.env.DB_URL;     // URL of our database 
+
+   mongoose.connect(DB_URL,{       // common struture use always in database connection
+      useNewUrlParser : true,
+      useUnifiedTopology : true,
+      useFindAndModify : false,
+   });
+   const db=mongoose.connection;   //storing our moongose connection
+   db.on('error',console.error.bind(console , 'connection error : '));
+   db.once('open',()=>{
+       console.log('DB Connected ....');
+   });
+}
