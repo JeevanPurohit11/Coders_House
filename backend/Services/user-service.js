ea91eb6e094f49aeec6263ff1e26b3f6ad3e4276
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-10 20:39:45 +0530

    all dependences

--- backend/Services/user-service.js
+++ backend/Services/user-service.js
@@ -1,5 +1,16 @@
-class userService{
+const UserModel = require('../models/user-model');
+
+ class userService{
+      
+    async findUser(filter){
+        const user = await UserModel.findOne(filter);     //findOne is inbuld mongodb as a method.
+        return user;
+    }
+    async createUser(data){
+        const user = await UserModel.create(data);     //create is inbuld mongodb as a method.
+        return user;
+    }
 
 }
 
-module.exports = userService();
+module.exports = new userService();
