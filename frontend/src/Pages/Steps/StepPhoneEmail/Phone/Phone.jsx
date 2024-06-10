a509741fe2d65e637ef9e1db09efbfb9204c388e
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-10 23:57:31 +0530

    sending request to server will enter ph number

--- frontend/src/Pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/Pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -3,13 +3,17 @@ import Card from '../../../../Components/Shared/Card/Card';
 import Button from '../../../../Components/Shared/Button/Button';
 import TextInput from '../../../../Components/Shared/TextInput/TextInput';
 import Styles from '../StepPhoneEmail.module.css';
-
+import { sendOtp } from '../../../../Components/Shared/http';   //{} beacuse not default export 
 
 //onNext is a function which we can call from our parent component which is this case is StepPhoneEmail.jsx
 const Phone = ({onNext}) => {
 
   const [phoneNumber,setPhoneNumber]=useState('');
-
+   async function submit(){                          //bacially api call return promise
+        const res= await sendOtp({phone : phoneNumber});
+        console.log(res);
+        //onNext();
+    }
     return (
         <div >
           <Card title="Enter Your Phone Number " icon="phone.png">
@@ -17,7 +21,7 @@ const Phone = ({onNext}) => {
             <TextInput value={phoneNumber} onChange={(e)=>setPhoneNumber(e.target.value)}/> 
         <div>
           <div className={Styles.actionButtonWrap}>
-             <Button  onClick={onNext} text="Next " />   
+             <Button  onClick={submit} text="Next " />   
           </div>
           <p className={Styles.buttonParagraph}>
                    By entering your number, you’re agreeing to our Terms of Service and Privacy Policy. Thanks!
