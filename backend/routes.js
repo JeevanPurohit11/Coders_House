9ceb29be2bd6f01026cd8ecc9c4925c7f10cd0ea
JeevanPurohit11 <jivanpurohit1106@gmail.com>
2024-06-14 23:35:41 +0530

    added actvate route

--- backend/routes.js
+++ backend/routes.js
@@ -1,13 +1,16 @@
 //all our routes will be store here
 //creating router (Router is bascially a function)
-const router=require('express').Router();   // our express router has been created inside router.
+// const router=require('express').Router();   // our express router has been created inside router.
 const authController = require('./Controllers/auth-controller');  //imported
-
+const router = require('express').Router();
+const activateController = require('./Controllers/activate-controller');
 
 
  //our first request would be post request.
 router.post('/api/send-otp', authController.sendOtp);
 router.post('/api/verify-otp',authController.verifyOtp);
+router.post('/api/activate',activateController.activate);
 
 
 module.exports=router;
+
