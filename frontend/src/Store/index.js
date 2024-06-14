75dc4464fceb42e8e9f3a6c6b4f026dd57e4a09b
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-14 23:52:16 +0530

    added activate parth

--- frontend/src/Store/index.js
+++ frontend/src/Store/index.js
@@ -2,9 +2,11 @@
 //now we have to tell react to use this store
 import { configureStore } from '@reduxjs/toolkit'
 import auth from './authSlice';
+import activate from './activateSlice';
 
 export const store = configureStore({
   reducer: {
       auth,
+      activate,
   },
-})
+});
