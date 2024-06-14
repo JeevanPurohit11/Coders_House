0bbe2a945245ce27cd7a1143fea3fdb1d8c9ab62
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-14 23:53:49 +0530

    added new package + style

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
 require('dotenv').config();  // Use credentials from .env file
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5500;
-const bodyParser = require('body-parser');
 const DbConnect = require('./database');
 const cors = require('cors');
+// Connect to the database
+DbConnect();
+// Import and use router
+const router = require('./routes');
+app.use(router);  // Prefix the routes with /api
 
 const corsOptions = {
+    Credential : true,                 //for getting cookie //tokens easily
     origin: ['http://localhost:3000'],  // Allow requests from the frontend
 };
 
 app.use(cors(corsOptions));
-app.use(bodyParser.json());  // Enable JSON middleware
+app.use(express.json());  // Enable JSON middleware
 
-// Connect to the database
-DbConnect();
 
 // Basic route for testing
 app.get('/', (req, res) => {
     res.send('Hello from express Js');
 });
 
-// Import and use router
-const router = require('./routes');
-app.use('/api', router);  // Prefix the routes with /api
 
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
