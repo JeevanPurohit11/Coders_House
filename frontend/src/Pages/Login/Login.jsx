422b872cfee26755efb7b1f7677f5271a4ba0a76
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-14 23:51:09 +0530

    not require + for now

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,28 +1,28 @@
-//we are not using this component as we created activte and authenticate.
-import React,{useState} from 'react'
-import styles from './Login.module.css'
-import StepPhoneEmail from '../Steps/StepPhoneEmail/StepPhoneEmail'
-import StepOtp from '../Steps/StepOtp/StepOtp'
+// //we are not using this component as we created activte and authenticate.
+// import React,{useState} from 'react'
+// import styles from './Login.module.css'
+// import StepPhoneEmail from '../Steps/StepPhoneEmail/StepPhoneEmail'
+// import StepOtp from '../Steps/StepOtp/StepOtp'
 
-const steps = {
-    1 : StepPhoneEmail,
-    2 : StepOtp,
-}
+// const steps = {
+//     1 : StepPhoneEmail,
+//     2 : StepOtp,
+// }
 
-const Login = () => {
-    let [step,setstep]=useState(1);
-    let Step = steps[step];
+// const Login = () => {
+//     let [step,setstep]=useState(1);
+//     let Step = steps[step];
    
-    function onNext(){
-       setstep(step+1);
-    }
-  return (
-    <>
-    <div>
-        <Step onNext={onNext} />
-    </div>
-    </>
-  )
-}
+//     function onNext(){
+//        setstep(step+1);
+//     }
+//   return (
+//     <>
+//     <div>
+//         <Step onNext={onNext} />
+//     </div>
+//     </>
+//   )
+// }
 
-export default Login
+// export default Login
