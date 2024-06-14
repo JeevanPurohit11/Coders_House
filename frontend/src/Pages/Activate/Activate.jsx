57f99fe6a30ca0ec292803a4083fb08a974dbd40
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-14 23:47:42 +0530

    edited

--- frontend/src/Pages/Activate/Activate.jsx
+++ frontend/src/Pages/Activate/Activate.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import styles from './Activate.module.css'
+import React, { useState } from 'react'
+import Styles from './Activate.module.css'
+import StepName from '../Steps/StepName/StepName'
+import StepAvatar from '../Steps/StepAvatar/StepAvatar';
+
+
+const steps = {
+   1 : StepName,
+   2 : StepAvatar,
+};
 
 const Activate = () => {
+  const [step,setStep] =useState(1);
+  const Step = steps[step];
+
+  function onNext(){
+    setStep(step+1);
+  }
+
   return (
-    <div>Activate</div>
+    <div className='cardWrapper'>
+        <Step onNext={onNext}></Step>
+    </div>
   )
 }
 
