876578030d01e5c77a2741feff53cca546b9d71f
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-16 19:54:10 +0530

    removed environment variable + static API

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Creating axios instance with configuration
 const api = axios.create({
-    baseURL: 'http://localhost:5500',
+    baseURL: "http://localhost:5500",
     withCredentials: true,
     headers: {
         'Content-type': 'application/json',
@@ -11,6 +11,7 @@ const api = axios.create({
 });
 
 // List of all endpoints using the base URL from the axios instance
+
 export const sendOtp = (data) => api.post('http://localhost:5500/api/send-otp', data);
 export const verifyOtp = (data) => api.post('http://localhost:5500/api/verify-otp', data);
 
