6c094926b5f0c1a770496a55fa07329a25a5f14c
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-16 19:53:10 +0530

    solved error

--- frontend/src/Components/Shared/TextInput/TextInput.jsx
+++ frontend/src/Components/Shared/TextInput/TextInput.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Styles from './TextInput.module.css'
 
-const TextInput = ({props}) => {
+const TextInput = ({ value, onChange }) => {
   return (
-    <div >
-        <input className={Styles.input} type='text' {...props} />
+    <div>
+      <input
+        className={Styles.input}
+        type='text'
+        value={value}
+        onChange={onChange}
+      />
     </div>
-  )
-}
+  );
+};
+
 
 export default TextInput
