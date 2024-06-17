5f5a6f7d7ae83291f96d12e575724beb2c5dded6
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-17 23:50:08 +0530

    updated

--- backend/routes.js
+++ backend/routes.js
@@ -10,7 +10,7 @@ const authMiddleware = require('./Middlewares/auth-middleware');
  //our first request would be post request.
 router.post('/api/send-otp', authController.sendOtp);
 router.post('/api/verify-otp',authController.verifyOtp);
-router.post('/api/activate',authMiddleware,activateController.activate);
+//router.post('/api/activate',authMiddleware,activateController.activate);
 
 
 module.exports=router;
