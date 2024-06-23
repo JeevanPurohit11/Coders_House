0365e0bd902b3a9e3f364477c49bdc166bcda7ec
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-24 02:12:42 +0530

    user name/avatar if else

--- frontend/src/Components/Shared/Navigation/Navigation.jsx
+++ frontend/src/Components/Shared/Navigation/Navigation.jsx
@@ -23,6 +23,7 @@ import {setAuth} from '../../../Store/authSlice';
 
   const dispatch = useDispatch();
   const { isAuth, user } = useSelector((state) => state.auth);   //taking data from store
+
   async function logoutUser() {
       try {
           const { data } = await logout();     //auth controller, pass from middleware
@@ -39,20 +40,26 @@ import {setAuth} from '../../../Store/authSlice';
            <img src="/images/logo.png" alt="logo"/>
            <span style ={logoText}>Coders House</span>
         </Link>
-        {isAuth && (
+            {isAuth && (
                 <div className={Styles.navRight}>
-                    <h3>{user?.name}</h3> {/*if you exisit then show name*/}
+                    <h3>{user.name}</h3>
                     <Link to="/">
                         <img
                             className={Styles.avatar}
                             src={
-                                user.avatar } 
+                                user.avatar
+                                    ? user.avatar
+                                    : '/images/monkey-avatar.png'
+                            }
                             width="40"
                             height="40"
                             alt="avatar"
                         />
                     </Link>
-                    <button className={Styles.logoutButton} onClick={logoutUser}>
+                    <button
+                        className={Styles.logoutButton}
+                        onClick={logoutUser}
+                    >
                         <img src="/images/logout.png" alt="logout" />
                     </button>
                 </div>
