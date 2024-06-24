7a6ddcc54eb830b263cbeb3f28bdb349441a55f3
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-24 23:53:55 +0530

    dited for fullwidth 100 or not

--- frontend/src/Components/Shared/TextInput/TextInput.jsx
+++ frontend/src/Components/Shared/TextInput/TextInput.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Styles from './TextInput.module.css'
+import React from 'react';
+import Styles from './TextInput.module.css';
 
-const TextInput = ({ value, onChange }) => {
+const TextInput = ({ value = {}, onChange }) => {
+  const { fullwidth = 'false', text = '' } = value;
+
   return (
     <div>
       <input
         className={Styles.input}
+        style={{ width: fullwidth === 'true' ? '100%' : 'inherit' }}
         type="text"
-        value={value}
+        value={text}
         onChange={onChange}
       />
     </div>
   );
 };
 
-
-export default TextInput
+export default TextInput;
