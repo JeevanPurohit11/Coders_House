1edd67d4eab640f32a36c310978d743321f6d988
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-24 23:54:25 +0530

    added styles to page

---
+++ frontend/src/Components/AddRoomModel/AddRoomModel.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Styles from './AddRoomModel.module.css';
+import TextInput from '../Shared/TextInput/TextInput';
+const AddRoomModel = () => {
+  return (
+    <div className={Styles.modalMask}>
+        <div className={Styles.modalBody}>
+            <div className={Styles.modalHeader}>
+               <h3 className={Styles.heading}>Enter the topic to be disscussed.</h3>
+               <TextInput fullwidth="true" />
+                <h2 className={Styles.subHeading}>Room Types</h2>
+               <div className={Styles.roomTypes}>
+                   <div className={Styles.typeBox}>
+                        <img src="/images/globe.png" alt="globe" />
+                        <span>Open</span>
+                   </div>
+                   <div className={Styles.typeBox}>
+                        <img src="/images/social.png" alt="social" />
+                        <span>Open</span>
+                   </div>
+                   <div className={Styles.typeBox}>
+                        <img src="/images/lock.png" alt="lock" />
+                        <span>Open</span>
+                   </div>
+               </div>
+            </div>
+            <div className={Styles.modalFooter}>
+                 <h2>Start a room , open to everyone.</h2>
+                 <button><img src="/images/celebration.png" alt="celebration" />Let's Go </button>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default AddRoomModel
