26ef538f9e040a3a903a43bbe8fbdd9ebd0a2478
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-25 23:56:21 +0530

    room api added

--- backend/routes.js
+++ backend/routes.js
@@ -5,7 +5,7 @@ const authController = require('./Controllers/auth-controller');  //imported
 const router = require('express').Router();
 const activateController = require('./Controllers/activate-controller');
 const authMiddleware = require('./Middlewares/auth-middleware');
-
+const roomController=require('./Controllers/room-Controller')
 
  //our first request would be post request.
 router.post('/api/send-otp', authController.sendOtp);
@@ -14,5 +14,7 @@ router.post('/api/activate',authMiddleware,activateController.activate);
 router.get('/api/refresh',authController.refresh);   //get request
 // router.post('/api/logout',authMiddleware,authController.logout); //we make this request if user was login , if no user login then we cannot able to make this request so we use middleware to check if user login or not 
 router.post('/api/logout', authMiddleware, authController.logout);
+router.post('api/rooms',authMiddleware , roomController.create);
+
 module.exports=router;
 
