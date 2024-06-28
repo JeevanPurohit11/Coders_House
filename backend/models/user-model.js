565fec9f2f8e94a6ca3b17484dad5d68bf230e6f
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-28 23:53:57 +0530

    avatar dynamically change

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -5,11 +5,21 @@ const Schema = mongoose.Schema;
 const userSchema= new Schema({
     phone : {type : String , required : true},
     name : {type : String , required : false},    // while writing name still activated was false only.
-    avatar :{ type : String ,required :false},
+    avatar: {
+      type: String,
+      required: false,
+      get: (avatar) => {
+          if (avatar) {
+              return `${process.env.BASE_URL}${avatar}`;
+          }
+          return avatar;
+      },
+  },
     activated : { type : Boolean , required : false , default : false},
 },
   {
     timestamps: true,
+    toJSON : {getters : true},
   }
 );
                            //model Name , used Schema , table/connection name
