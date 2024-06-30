ba80f15eedc4ed17e7d0eed0b961a5dcba6dec68
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-06-30 21:57:22 +0530

    added client function

--- frontend/src/hooks/useWebRTC.js
+++ frontend/src/hooks/useWebRTC.js
@@ -1,3 +1,17 @@
-async function useWebRTC{
+import { useState } from "react"
+//we have to create a custom hook useStateWithCallBack , beacuse here useEffect will not work, as we have to do 
+export const useWebRTC=()=>{
+    const [clients,setClients]=useState([
+        {
+            id:1,
+            name : 'Rakesh Rajpurohit',
+        },
+        {
+            id:2,
+            name : 'Bhavesh Rajpurohit',
+        },
+
+    ]);
+    return {clients};
 
 }
