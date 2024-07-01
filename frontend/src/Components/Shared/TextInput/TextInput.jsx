ebe0a303c23bd127c79e7e443124fbd09c348418
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-07-01 23:43:17 +0530

    added sockket functionality

--- frontend/src/Components/Shared/TextInput/TextInput.jsx
+++ frontend/src/Components/Shared/TextInput/TextInput.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Styles from './TextInput.module.css';
 
-const TextInput = ({ value = '', fullwidth = 'false', onChange }) => {
+const TextInput = ({ value = '', fullWidth = 'false', onChange }) => {
   return (
     <div>
       <input
         className={Styles.input}
-        style={{ width: fullwidth === 'true' ? '100%' : 'inherit' }}
+        style={{ width: fullWidth === 'true' ? '100%' : 'inherit' }}
         type="text"
         value={value}
         onChange={onChange}
--- frontend/src/hooks/useStateWithCallBack.js
+++ frontend/src/hooks/useStateWithCallBack.js
@@ -1,14 +1,21 @@
-export const useStateWithCallBack=()=>{
-    // useState([
-    //     {
-    //         id:1,
-    //         name : 'Rakesh Rajpurohit',
-    //     },
-    //     {
-    //         id:2,
-    //         name : 'Bhavesh Rajpurohit',
-    //     },
+import { useCallback,useRef, useEffect, useState } from "react"
 
-    // ]);
-    // return {clients};
+export const useStateWithCallBack=(initialState)=>{
+    const [state,setState]=useState(initialState);
+    const cbRef= useRef();  //we are using cbRef when we not what to render our page on state change.
+
+    const updateState =useCallback((newState,cb)=>{
+        cbRef.current=cb; //just storing the callback
+        
+        setState((prev)=>{
+            return typeof newState === 'function' ? newState(prev) : newState;
+        });
+    },[]);
+    useEffect(()=>{
+        if(cbRef.current){
+            cbRef.current(state);
+            cbRef.current=null;
+        }
+    },[state]);
+    return [state,updateState];
 }
