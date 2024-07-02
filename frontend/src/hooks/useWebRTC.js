719de070ff15e8f6fa5e188a9e92c94177417538
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-07-02 22:56:17 +0530

    socket connection

--- frontend/src/hooks/useWebRTC.js
+++ frontend/src/hooks/useWebRTC.js
@@ -1,7 +1,9 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useStateWithCallBack } from "./useStateWithCallBack";
 import {socketInit} from '../Pages/socket/index'
-import ACTIONS from "../actions";
+import { ACTIONS } from "../actions";
+import freeice from 'freeice';
+
 
 // const users = [
 //     {
@@ -28,7 +30,7 @@ export const useWebRTC = (roomId, user) => {
         audioElements.current[userId] = instance;
     };
 
-    const addNewClients = useCallback((newClient, cb) => {
+    const addNewClient = useCallback((newClient, cb) => {
         const lookingFor = clients.find((client) => client.id === newClient.id);
         if (lookingFor === undefined) {
             setClients((existingClients) => [...existingClients, newClient], cb);
@@ -36,8 +38,89 @@ export const useWebRTC = (roomId, user) => {
     }, [clients, setClients]);
 
     useEffect(()=>{
-         socket.current=socketInit();
-    },[])
+          const handleNewPeer=async({roomId,createOffer, user:remoteUser})=>{
+                   //if already connected then give warning
+                   if(peerId in connections.current){
+                         return console.warn(`you are already connected with id ${peerId} (${user.name})`);
+                   }
+                   connections.current[peerId]=new RTCPeerConnection({
+                      iceServers : freeice(),  //connection created.
+                   })
+                   //handle all new ice candidate
+                   connections.current[peerId].onicecandidate=(event)=>{
+                    socket.current.emit(ACTIONS.RELAY_ICE,{
+                        peerId,
+                        icecandidate : event.candidate
+                    })
+                   }
+                   //handle ontrack on this connection
+                   connections.current[peerId].ontrack = ({
+                    streams: [remoteStream],
+                }) => {
+                    addNewClient({ ...remoteUser, muted: true }, () => {
+                        // get current users mute info
+                        const currentUser = clientsRef.current.find(
+                            (client) => client.id === user.id
+                        );
+                        if (currentUser) {
+                            socket.current.emit(ACTIONS.MUTE_INFO, {
+                                userId: user.id,
+                                roomId,
+                                isMute: currentUser.muted,
+                            });
+                        }
+                        if (audioElements.current[remoteUser.id]) {
+                            audioElements.current[remoteUser.id].srcObject =
+                                remoteStream;
+                        } else {
+                            let settled = false;
+                            const interval = setInterval(() => {
+                                if (audioElements.current[remoteUser.id]) {
+                                    audioElements.current[
+                                        remoteUser.id
+                                    ].srcObject = remoteStream;
+                                    settled = true;
+                                }
+
+                                if (settled) {
+                                    clearInterval(interval);
+                                }
+                            }, 300);
+                        }
+                    });
+                };
+                  // Add connection to peer connections track
+                  localMediaStream.current.getTracks().forEach((track) => {
+                    connections.current[peerId].addTrack(
+                        track,
+                        localMediaStream.current
+                    );
+                });
+
+                // Create an offer if required
+                if (createOffer) {
+                    const offer = await connections.current[
+                        peerId
+                    ].createOffer();
+
+                    // Set as local description
+                    await connections.current[peerId].setLocalDescription(
+                        offer
+                    );
+
+                    // send offer to the server
+                    socket.current.emit(ACTIONS.RELAY_SDP, {
+                        peerId,
+                        sessionDescription: offer,
+                    });
+                }
+            }
+
+
+          }
+         socket.current.on(ACTIONS.ADD_PEER,handleNewPeer)
+         socket.current=socketInit()
+    },[]);
     // Capture audio
     useEffect(() => {
         const startCapture = async () => {
@@ -48,7 +131,7 @@ export const useWebRTC = (roomId, user) => {
 
         if (user && user.id) {
             startCapture().then(() => {
-                addNewClients(user, () => {
+                addNewClient(user, () => {
                     const localElement = audioElements.current[user.id];
                     if (localElement) {
                         localElement.volume = 0;  //if we comment this then we can able to listen sound
@@ -61,7 +144,7 @@ export const useWebRTC = (roomId, user) => {
         } else {
             console.error("User is undefined or does not have an id");
         }
-    }, [user, addNewClients]);
+    }, [user, addNewClient]);
 
     return { clients, provideRef };
 };
